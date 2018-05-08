a5646a316eea7be2b19b6f5a06026f2fdf7228d9
Kevin McAnoy <kmcanoy@redhat.com>
2018-05-08 16:20:22 -0400

    prep for git

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,8 @@ LABEL io.k8s.description="A container for cleaning the slate of target Deploymen
 ADD scripts/clean-slate.sh /opt/app/scripts/
 
 RUN microdnf --enablerepo=rhel-7-server-rpms \
-			install tar gzip --nodocs ;\
+			install git --nodocs ;\
 			microdnf clean all ;\
-			curl --retry 5 -Lso /tmp/client-tools.tar.gz https://github.com/openshift/origin/releases/download/v3.6.0/openshift-origin-client-tools-v3.6.0-c4dd4cf-linux-64bit.tar.gz ;\
-			tar zxf /tmp/client-tools.tar.gz --strip-components=1 -C /usr/local/bin
 			
 RUN	chgrp -R 0 /opt/app/scripts/ ;\
 	chmod -R g+rw /opt/app/scripts/ ;\
--- scripts/clean-slate.sh
+++ scripts/clean-slate.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
 
-if [ -f /run/secrets/kubernetes.io/serviceaccount/token ]; then
-	TOKEN=`cat /run/secrets/kubernetes.io/serviceaccount/token`
-	PROJECT=`cat /run/secrets/kubernetes.io/serviceaccount/namespace`
-	CA=/run/secrets/kubernetes.io/serviceaccount/ca.crt
-else
-	echo "No token found. Are you running on OpenShift?"
-fi
-
-# Make sure we're logged in
-if [ -n "$TOKEN" ]; then
-	echo "Authenticating with token"
-	oc login $KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT --token=$TOKEN --certificate-authority=$CA
-fi
-
-# Iterate through DCs, and execute a rollout on them
-if [ -n "$DEPLOYMENT_CONFIGS" ]; then
-	for dc in $(echo $DEPLOYMENT_CONFIGS | sed "s/,/ /g")
-	do
-		echo "--"
-		echo "Wiping the slate of $dc"
-		oc rollout latest dc/$dc -n $PROJECT
-		echo "Done"
-	done
-else
-	echo "No DeploymentConfigs specified. Skipping execution."
-fi
+echo "This is the job"
+git --version 
